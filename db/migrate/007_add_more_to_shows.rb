ab5141944e13242ee197be2a3f428f2776a8084a
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-10 18:21:18 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_more_to_shows.rb
+++ db/migrate/007_add_more_to_shows.rb
@@ -1,4 +1,4 @@
-class AddMoreToShows < ActiveRecord::Migration[5.2]
+class AddMoreToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
